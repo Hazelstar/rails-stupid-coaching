20c2ef24820bf7f454edd593c9d01230581b604b
Lola Guerin <lola.guerin@gmail.com>
2020-11-02 15:08:12 +0000

    Was stuck on an error relative to nvm version for about 2 hours. Completed the exercise once I was helped to find the rigth version

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,20 @@
 class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:answer]
+
+    'Great!' if @question == 'i am going to work!'
+    if @question.end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = 'I don\'t care, get dressed and go to work!'
+    end
+  end
 end
+
+# @questions = ['i am going to work!', '?']
+#     if params[:question]
+#       @question = @question.select { |question| question.end_with?(params[:question]) }
+#     end
